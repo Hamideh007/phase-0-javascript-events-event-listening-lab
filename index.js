73ceec09617ad0005d14d794518e1d2cdba484d3
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-03-30 23:45:39 -0400

    complete java event

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    const button = document.querySelector("#button"); // select the button element by ID
+    button.addEventListener("click", function(event) {
+      alert("I was clicked!");
+    });
+  }
+  
+  
